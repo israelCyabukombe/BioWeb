6e252cce674ca5a5acc360b10f26ce3744c3f835
israel <israel.cyabu93@gmail.com>
2025-11-04 20:42:36 -0600

    Add Skills page and react routing

--- bioweb.client/src/App.jsx
+++ bioweb.client/src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import './components/Navbar.jsx'
+import './App.css';
+import './components/Navbar.jsx';
 import MyNavbar from './components/Navbar.jsx';
-import About from './components/About.jsx'
+import { Routes, Route } from 'react-router-dom';;
+import About from './components/About.jsx';
+import Skills from './components/Skills.jsx';
+import Contact from './components/Contact.jsx';
 
 function App() {
 
     return (
         <>
-            <MyNavbar />
-            <About/>
+        <MyNavbar />
+            <Routes>
+                <Route path="/" element={<About />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/contact" element={<Contact />} />
+        </Routes>
         </>
     );
 }
