4471eeb29c6f4a7fbcecebbed9e2816be14075ee
Israel Cyabukombe <israelcyabu93@gmail.com>
2026-06-17 20:46:54 -0500

    Feature/add api token end point (#49)
    * Add Token Endpoint to generate JWT
    
    * Add Token Endpoint to generate JWT
    
    ---------
    
    Co-authored-by: israel <israel.cyabu93@gmail.com>

--- BioWeb.Server/Program.cs
+++ BioWeb.Server/Program.cs
@@ -1,17 +1,28 @@
 using BioWeb.Server.Data;
+using BioWeb.Server.Models;
 using BioWeb.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+}
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 //register db connection
-
 var connectionString = builder.Configuration.GetConnectionString("BioWebDb");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -32,7 +43,7 @@ var corsPolicy = "_myAllowSpecificOrigins";
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name:corsPolicy, builder =>
+    options.AddPolicy(name: corsPolicy, builder =>
         builder.WithOrigins(
             "http://localhost:5174",
             "http://localhost:5173",
@@ -43,8 +54,30 @@ builder.Services.AddCors(options =>
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
-    );      
+    );
 });
+
+var jwtKey = builder.Configuration["Jwt:Key"]!;
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtKey))
+    };
+});
+
+builder.Services.Configure<MyBioSettings>(builder.Configuration.GetSection("MyBioSettings"));
 
 var app = builder.Build();
 
@@ -100,6 +133,52 @@ app.MapGet("/api/projects", async (IBiographyService service) =>
 })
 .WithName("GetProjects")
 .WithOpenApi();
+
+//token
+app.MapPost("/api/token",
+    [AllowAnonymous] (
+    BioWeb.Server.Models.User user,
+    IOptions<MyBioSettings> myBioSettings
+) =>
+{
+    bool connected = false;
+
+    if (myBioSettings.Value.Username == user.Username &&
+        myBioSettings.Value.Password == user.Password)
+    {
+        connected = true;
+    }
+    if (connected == true)
+    {
+        var jwtKey = config.GetValue<string>("Jwt:Key")!;
+        var key = Encoding.ASCII.GetBytes(jwtKey);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Username),
+                new Claim(JwtRegisteredClaimNames.Email, user.Username),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+            }),
+
+            Expires = DateTime.UtcNow.AddMinutes(60),
+            SigningCredentials = new SigningCredentials
+            (new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha512Signature)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var stringToken = tokenHandler.WriteToken(token);
+
+        return Task.FromResult(Results.Ok(new { token = stringToken }));
+    }
+
+    return Task.FromResult(Results.Unauthorized());
+});
+ 
 
 
 app.MapFallbackToFile("/index.html");
