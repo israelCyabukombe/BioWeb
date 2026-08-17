5f38aae7c0267b4abeb56468c355b69820e70a6f
israel <israel.cyabu93@gmail.com>
2026-08-16 22:28:29 -0500

    update cors add https://icyabu.com

--- BioWeb.Server/Program.cs
+++ BioWeb.Server/Program.cs
@@ -50,7 +50,8 @@ builder.Services.AddCors(options =>
             "http://localhost:4173",
             "https://icy-desert-022716310.7.azurestaticapps.net",
             "https://bioweb-server-fkf6c7b0d2dkdede.centralus-01.azurewebsites.net",
-            "https://www.icyabu.com"
+            "https://www.icyabu.com",
+            "https://icyabu.com"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
